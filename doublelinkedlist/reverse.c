7662835649c2430ee196572caf7eebe2dd266574
jaishubhakanth999 <jaishubhakanth999@gmail.com>
2026-02-25 11:08:20 +0530

    push

---
+++ doublelinkedlist/reverse.c
@@ -0,0 +1,18 @@
+// struct node *reverse(struct node *head)
+// {
+//     struct node *current = head;
+//     struct node *temp = NULL;
+//     if(head = NULL)
+//     {
+//         return NULL;
+
+//     }
+//     while(current != NULL)
+//     {
+//         temp = current -> prev;
+//         current -> prev = current -> next;
+//         current -> next = temp;
+//         current = current -> next;
+//     }
+//     return head;
+// }
